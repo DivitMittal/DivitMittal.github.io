9654d545f958d88a7a932a7b963c98f04c8189b7
DivitMittal <64.69.76.69.74.m@gmail.com>
2024-12-20 02:29:09 +0530

    Updates

---
+++ _app/immutable/chunks/cursor.af94b48d.js
@@ -0,0 +1,1 @@
+import{w as t}from"./index.203dd0cf.js";const{subscribe:r,set:c,update:s}=t(0),o={subscribe:r,increment:()=>s(e=>e<1?e+.15:e),decrement:()=>s(e=>e-.1),reset:()=>c(0)},b=t(0);export{o as b,b as s};
